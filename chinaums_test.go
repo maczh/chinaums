5a3fb599700038efb03ff24d29801eebe5219c5e
maczh <maczh@qq.com>
2025-09-23 18:07:11 +0800

    BSC扫码部分已经完成

--- chinaums_test.go
+++ chinaums_test.go
@@ -1,7 +1,96 @@
 package chinaums
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestGenNonce(t *testing.T) {
 	t.Log(generateNonce())
+}
+
+func TestBSCPay(t *testing.T) {
+	req := BSCPayReq{
+		TransactionAmount: 2,
+		MerchantOrderId:   "JHTEST" + time.Now().Format("20060102150405"),
+		MerchantRemark:    "测试订单",
+		PayMode:           PayModeScan,
+		PayCode:           "133880443140539914",
+		SrcReserved:       "备注信息",
+		OrderDesc:         "火锅消费订单(寄海火锅福州东街口店)",
+		StoreId:           "ae48ee12-f901-4cbb-ba0d-4c7497a26c23",
+		OperatorId:        "",
+		Goods:             nil,
+		Longitude:         "+119.299312",
+		Latitude:          "+26.087587",
+	}
+	resp, err := Client.BscPay.Pay(&req)
+	if err != nil {
+		t.Log("支付接口调用失败:", err)
+		return
+	}
+	t.Log("支付结果:", ToJSONPretty(resp))
+}
+
+func TestBSCVoidPay(t *testing.T) {
+	req := BSCVoidPayReq{
+		MerchantOrderId: "JHTEST20250923160600",
+		OriginalOrderId: "20250923160600406370168105",
+		MerchantRemark:  "测试取消订单",
+	}
+	resp, err := Client.BscPay.VoidPay(&req)
+	if err != nil {
+		return
+	}
+	t.Log("取消支付结果:", ToJSONPretty(resp))
+}
+
+func TestBSCRefund(t *testing.T) {
+	requestId, _ := generateNonce()
+	req := BSCRefundReq{
+		MerchantOrderId:   "JHTEST20250923164207",
+		OriginalOrderId:   "20250923164207406370624529",
+		MerchantRemark:    "测试退款订单",
+		RefundRequestId:   requestId,
+		TransactionAmount: 1,
+		Goods:             nil,
+		StoreId:           "",
+		OperatorId:        "",
+		RefundDesc:        "测试退款",
+	}
+	resp, err := Client.BscPay.Refund(&req)
+	if err != nil {
+		t.Log("退款接口调用失败:", err)
+		return
+	}
+	t.Log("退款结果:", ToJSONPretty(resp))
+}
+
+func TestBSCQuery(t *testing.T) {
+	req := BSCQueryReq{
+		MerchantOrderId: "JHTEST20250923164207",
+		OriginalOrderId: "20250923164207406370624529",
+		OriTransDate:    "20250923",
+	}
+	resp, err := Client.BscPay.Query(&req)
+	if err != nil {
+		t.Log("查询接口调用失败:", err)
+		return
+	}
+	t.Log("查询结果:", ToJSONPretty(resp))
+}
+
+func TestBSCRefundQuery(t *testing.T) {
+	req := BSCRefundQueryReq{
+		MerchantOrderId: "JHTEST20250923164207",
+		OriginalOrderId: "20250923164207406370624529",
+		OriTransDate:    "20250923",
+		RefundRequestId: "63a9055b56945b5864f051735f678751",
+	}
+	resp, err := Client.BscPay.RefundQuery(&req)
+	if err != nil {
+		t.Log("退款查询接口调用失败:", err)
+		return
+	}
+	t.Log("退款查询结果:", ToJSONPretty(resp))
 }
